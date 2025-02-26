9f12fd33d4a78f0c3aa6e7d40022e4050533f291
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2025-02-26 00:26:25 +0000

    Fix project gantt chart

--- SpreadsheetUtility/SpreadsheetUtility.Library/Models/TaskDto.cs
+++ SpreadsheetUtility/SpreadsheetUtility.Library/Models/TaskDto.cs
@@ -9,10 +9,12 @@ namespace SpreadsheetUtility.Library
     public class TaskDto
     {
         public string Id { get; set; }
+        public string ProjectID { get; set; }
         public string ProjectName { get; set; }
         public string TaskName { get; set; }
         public double EstimatedEffortHours { get; set; }
         public string Dependencies { get; set; }
+        public string ProjectDependency { get; set; }
         public string Progress { get; set; }
     }
 

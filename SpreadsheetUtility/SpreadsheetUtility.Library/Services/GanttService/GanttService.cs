9d6b7e5333e7509beeb28796dc8c3df8be7f078b
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2025-03-08 11:38:46 +0000

    Move volders

--- SpreadsheetUtility/SpreadsheetUtility.Library/Services/GanttService/GanttService.cs
+++ SpreadsheetUtility/SpreadsheetUtility.Library/Services/GanttService/GanttService.cs
@@ -7,15 +7,7 @@ using System.Threading.Tasks;
 
 namespace SpreadsheetUtility.Services
 {
-    public interface IGanttService
-    {
-        string ProcessExcelDataTasksFromFile(string taskFilePath, string teamFilePath);
-        string ProcessExcelDataProjectsFromFile(string taskFilePath, string teamFilePath);
-        List<GanttTask> LoadTasksFromDtos(List<TaskDto> taskDtos);
-        List<DeveloperAvailability> LoadDeveloperAvailabilityFromDtos(List<DeveloperDto> taskDtos);
-        List<GanttTask> AssignProjectsFromDtos(List<TaskDto> taskDtos, List<DeveloperDto> developerDtos);
-        GanttChartAllocation CalculateGanttChartAllocationFromDtos(List<TaskDto> taskDtos, List<DeveloperDto> developerDtos);
-    }
+    
     public class GanttService : IGanttService
     {
         IGanttChartProcessor _ganttChartProcessor;

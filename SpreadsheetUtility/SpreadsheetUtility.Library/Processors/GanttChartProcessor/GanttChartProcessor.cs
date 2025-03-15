49c4c0c07674d08f38bff593c5da7292f4c2d22c
Jose Gomes <jose.fernando.ferreira.gomes@outlook.com>
2025-03-15 19:10:03 +0000

    Task assignment refactor

--- SpreadsheetUtility/SpreadsheetUtility.Library/Processors/GanttChartProcessor/GanttChartProcessor.cs
+++ SpreadsheetUtility/SpreadsheetUtility.Library/Processors/GanttChartProcessor/GanttChartProcessor.cs
@@ -435,45 +435,7 @@ namespace SpreadsheetUtility.Library
 
             if (preSortTasks)
             {
-                List<GanttTask> sortedTasks = projectTasks;
-                // Change Dependencies from empty string to "0" for sorting
-                foreach (var task in projectTasks)
-                {
-                    if (string.IsNullOrEmpty(task.Dependencies))
-                    {
-                        task.Dependencies = "0";
-                    }
-                }
-
-                // Sort tasks by dependencies and estimated effort hours
-                sortedTasks = projectTasks.OrderBy(t => t.Dependencies).ThenByDescending(t => t.EstimatedEffortHours).ToList();
-
-                // Create a mapping of original task IDs to new task IDs
-                var idMapping = new Dictionary<string, string>();
-                for (int i = currentMaximumTaskID; i < sortedTasks.Count; i++)
-                {
-                    var originalTaskId = sortedTasks[i].Id;
-                    var newTaskId = (i + 1).ToString();
-                    idMapping[originalTaskId] = newTaskId;
-                    sortedTasks[i].Id = newTaskId;
-
-                    if (sortedTasks[i].Dependencies == "0")
-                    {
-                        sortedTasks[i].Dependencies = "";
-                    }
-                    currentMaximumTaskID = int.Parse(newTaskId);
-                }
-
-                // Update the dependencies to the new sorted order
-                foreach (var task in sortedTasks)
-                {
-                    if (!string.IsNullOrEmpty(task.Dependencies) && idMapping.ContainsKey(task.Dependencies))
-                    {
-                        task.Dependencies = idMapping[task.Dependencies];
-                    }
-                }
-
-                projectTasks = sortedTasks;
+                projectTasks = PreSortTasks(projectTasks);
             }
 
             foreach (var task in projectTasks)
@@ -504,7 +466,51 @@ namespace SpreadsheetUtility.Library
                 assignedDeveloper.Tasks.Add(task);
                 assignedDeveloper.SetNextAvailableDate(taskEnd.AddDays(1));                
             }
-        }        
+        }
+
+        private List<GanttTask> PreSortTasks(List<GanttTask> projectTasks)
+        {
+            List<GanttTask> sortedTasks = projectTasks;
+            // Change Dependencies from empty string to "0" for sorting
+            foreach (var task in projectTasks)
+            {
+                if (string.IsNullOrEmpty(task.Dependencies))
+                {
+                    task.Dependencies = "0";
+                }
+            }
+
+            // Sort tasks by dependencies and estimated effort hours
+            sortedTasks = projectTasks.OrderBy(t => t.Dependencies).ThenByDescending(t => t.EstimatedEffortHours).ToList();
+
+            // Create a mapping of original task IDs to new task IDs
+            var idMapping = new Dictionary<string, string>();
+            for (int i = currentMaximumTaskID; i < sortedTasks.Count; i++)
+            {
+                var originalTaskId = sortedTasks[i].Id;
+                var newTaskId = (i + 1).ToString();
+                idMapping[originalTaskId] = newTaskId;
+                sortedTasks[i].Id = newTaskId;
+
+                if (sortedTasks[i].Dependencies == "0")
+                {
+                    sortedTasks[i].Dependencies = "";
+                }
+                currentMaximumTaskID = int.Parse(newTaskId);
+            }
+
+            // Update the dependencies to the new sorted order
+            foreach (var task in sortedTasks)
+            {
+                if (!string.IsNullOrEmpty(task.Dependencies) && idMapping.ContainsKey(task.Dependencies))
+                {
+                    task.Dependencies = idMapping[task.Dependencies];
+                }
+            }
+
+            projectTasks = sortedTasks;
+            return projectTasks;
+        }
 
         private DateTime CalculateEndDate(DateTime start, double workDays, List<(DateTime Start, DateTime End)?>? vacations)
         {
